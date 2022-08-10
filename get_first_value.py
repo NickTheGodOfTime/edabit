16454505b1a58c21156c7995f76a0eaf4f851b40
NickTheGodOfTime <110388190+NickTheGodOfTime@users.noreply.github.com>
2022-08-10 15:32:16 +0530

    check

---
+++ get_first_value.py
@@ -0,0 +1,10 @@
+def get_first_value(number_list):
+    return number_list[0]
+
+
+e = get_first_value([1, 2, 3])
+print(e)
+e = get_first_value([80, 5, 100])
+print(e)
+e = get_first_value([-500, 0, 50])
+print(e)
