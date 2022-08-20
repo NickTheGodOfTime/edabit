d8099d7609cc6d56fd0fe3afd78052ffece1eecd
NickTheGodOfTime <110388190+NickTheGodOfTime@users.noreply.github.com>
2022-08-20 20:08:04 +0530

    Create secondMax.py

---
+++ secondMax.py
@@ -0,0 +1,22 @@
+def second_max(list):
+    index = 1
+    max = list[0]
+    secondMax = -1
+    
+    while index < len(list):
+        if max > list[index]:
+            secondMax = max
+            max = list[index]
+        elif 
+        
+        index = index + 1
+        
+    
+    return secondMax
+
+
+max = second_max([5, 6, 99, 10, 3, 67, 45, 112])
+print(max)
+
+max = second_max([5, 6, 99, 10, 3, 67, 45, 112, 100])
+print(max)
